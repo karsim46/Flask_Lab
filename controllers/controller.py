12e826353edac1c4a443dc44264754134ca259d7
Simon <simonwatson176@gmail.com>
2021-06-30 22:38:52 +0100

    2nd page link

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from app import app
 from models.games_list import orders
+from models.games_list import orders_1
 
 @app.route('/')
 def index():
     return render_template('index.html', title='SSC Game shop', orders=orders)
+
+@app.route('/order')
+def order():
+    return render_template('order.html', title='SSC Game shop', orders_1=orders_1)
+        
--- models/games_list.py
+++ models/games_list.py
@@ -2,8 +2,10 @@ from models.order import *
 
 
 
-Customer_1 = Order("Stephen", "Warhammer","30/06/21",1)
-Customer_2 = Order("Simon", "GTA5", "26/06/21",2)
-Customer_3 = Order("Craig", "GTA5", "25/06/21",1)
+Customer_1 = Order("Stephen", "Warhammer","30/06/21","qty 1")
+Customer_2 = Order("Simon", "GTA5", "26/06/21","qty 2")
+Customer_3 = Order("Craig", "GTA4", "25/06/21","qty 1")
+Customer_4 = Order("Sergio","Transformers","21/06/21","qty 1")
 orders = [Customer_1, Customer_2, Customer_3]
+orders_1= [Customer_4]
 
