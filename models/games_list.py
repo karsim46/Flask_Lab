f0af3728f0bf97244491e7c8a4795391c37f662b
Stephen <stephendcorcoran@gmail.com>
2021-06-30 17:00:20 +0100

    SSC Game Lab

--- models/games_list.py
+++ models/games_list.py
@@ -4,6 +4,6 @@ from models.order import *
 
 Customer_1 = Order("Stephen", "Warhammer","30/06/21",1)
 Customer_2 = Order("Simon", "GTA5", "26/06/21",2)
-Customer_3 = Order("Criag", "GTA5", "25/06/21",1)
+Customer_3 = Order("Craig", "GTA5", "25/06/21",1)
 orders = [Customer_1, Customer_2, Customer_3]
 
